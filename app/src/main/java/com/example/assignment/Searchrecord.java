0aff9033b16850bf83d645aaad59079472cb7110
Luqman Khan <kuqmankhan.bangash@gmail.com>
2022-04-12 21:13:44 +0500

    First commit

--- app/src/main/java/com/example/assignment/Searchrecord.java
+++ app/src/main/java/com/example/assignment/Searchrecord.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Right_fragment#newInstance} factory method to
+ * Use the {@link SearchRecord#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Right_fragment extends Fragment {
+class SearchRecord extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class Right_fragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Right_fragment() {
+    public SearchRecord() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class Right_fragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
+     * @return A new instance of fragment SearchRecord.
      */
     // TODO: Rename and change types and number of parameters
-    public static Right_fragment newInstance(String param1, String param2) {
-        Right_fragment fragment = new Right_fragment();
+    public static SearchRecord newInstance(String param1, String param2) {
+        SearchRecord fragment = new SearchRecord();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class Right_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.rightfragment, container, false);
+        return inflater.inflate(R.layout.fragment_search_record, container, false);
     }
 }
---
+++ app/src/main/java/com/example/assignment/SharedViewModel.java
@@ -0,0 +1,21 @@
+package com.example.assignment;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SharedViewModel extends ViewModel {
+    private MutableLiveData<String> mutableLiveData;
+
+    public void initialize(){
+        mutableLiveData = new MutableLiveData<String>();
+    }
+
+    public MutableLiveData<String> getMutableLiveData(){
+        return mutableLiveData;
+    }
+
+    public void setValue(String message){
+        mutableLiveData.setValue(message);
+    }
+}
+
